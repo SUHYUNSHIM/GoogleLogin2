a0ce12611c9257012fcb83532245f958f1062d34
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2019-10-09 22:48:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/googlelogin/DateCountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.googlelogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DateCountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_date_count)
+    }
+}
